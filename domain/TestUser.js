8f8e31c7c262bcdff50b7fb61ce908427e1e1fde
vcalatayud <lacuentadevictor@gmail.com>
2020-09-27 01:04:54 +0200

    added method to get a property value

--- domain/TestUser.js
+++ domain/TestUser.js
@@ -9,11 +9,52 @@ module.exports = async function(){
     var instance = Object.freeze({
         open,
         mustBeAbleTo,
+        getValueFor,
         close
     })
 
     async function open(url){
         await driver.get(url);
+    }
+
+    async function getValueFor(property){
+        var labels = await driver.findElements(By.tagName('label'))
+        var propertyLabels = await asyncFindAll(labels, async function(label){
+            var labelText = await label.getText()
+            return labelText.toLowerCase().replace(':','') == property.toLowerCase()
+        })
+
+        var relatedInput
+
+        if(propertyLabels.length == 0){
+            //Look for placeholders in inputs
+            var inputs = await driver.findElements(By.tagName('input'))
+            var relatedInputs = await asyncFindAll(inputs, async function(input){
+                var inputType = await input.getAttribute('type')
+                if(inputType == 'text'){
+                    var inputPlaceholder = await input.getAttribute('placeholder')
+                    return inputPlaceholder.toLowerCase() == property.toLowerCase()
+                }
+            })
+            
+            if(relatedInputs.length == 0) throw new Error(`Property "${property}" not found`)
+            relatedInput = relatedInputs[0]
+        }
+        else{
+            var propertyLabel = propertyLabels[0]
+            var relatedInputId = await propertyLabel.getAttribute('for')
+            var relatedInputs = await driver.findElements(By.id(relatedInputId))
+            if(relatedInputs.length == 0){
+                var propertyLabelText = await propertyLabel.getText()
+                throw new Error(`Missing input field for label "${propertyLabelText}"`)
+            }
+
+            relatedInput = relatedInputs[0]
+        }
+
+        var relatedInputValue = await relatedInput.getAttribute('value')
+    
+        return relatedInputValue
     }
 
     async function mustBeAbleTo(description){
@@ -26,7 +67,6 @@ module.exports = async function(){
 
         if(buttonOptions.length > 1) throw new Error(`There are several button options to ${description}`)
         var buttonOption = buttonOptions[0]
-
 
         var inputs = await driver.findElements(By.tagName('input'))
         var inputOptions = await asyncFindAll(inputs, async function(input){
--- test-integration/TestingHtmlView.spec.js
+++ test-integration/TestingHtmlView.spec.js
@@ -1,4 +1,5 @@
 const {TestUser} = require('../index')
+const {expect} = require('chai')
 const FakeServer = require('./FakeServer')
 const expectToThrow = require('expect-to-throw')
 
@@ -91,6 +92,85 @@ describe('testing html view', function(){
                 await expectToThrow(async function(){
                     await user.mustBeAbleTo('perform action')
                 })
+            })
+        })
+    })
+
+    describe.only('getValueFor', function(){
+        describe('when the property is in a label', function(){
+            it('must return the input value', async function(){
+                await server.setBody(`
+                    <label for="age">age</label>
+                    <input type="text" id="age" value="18">`)
+                await user.open(server.url)
+                var age = await user.getValueFor('age')
+                expect(age).to.equal('18')
+            })
+            it('must ignore case to match property and label', async function(){
+                await server.setBody(`
+                    <label for="age">Age</label>
+                    <input type="text" id="age" value="18">`)
+                await user.open(server.url)
+                var age = await user.getValueFor('agE')
+                expect(age).to.equal('18')
+            })
+            it('must ignore spaces to match property and label', async function(){
+                await server.setBody(`
+                    <label for="age"> age </label>
+                    <input type="text" id="age" value="18">`)
+                await user.open(server.url)
+                var age = await user.getValueFor('age')
+                expect(age).to.equal('18')
+            })  
+            it('must ignore colon to match property and label', async function(){
+                await server.setBody(`
+                    <label for="age"> Age: </label>
+                    <input type="text" id="age" value="18">`)
+                await user.open(server.url)
+                var age = await user.getValueFor('age')
+                expect(age).to.equal('18')
+            })
+            it('must work with several words', async function(){
+                await server.setBody(`
+                    <label for="date-of-birth"> Date of birth: </label>
+                    <input type="text" id="date-of-birth" value="1987-01-23">`)
+                await user.open(server.url)
+                var dateOfBirth = await user.getValueFor('date of birth')
+                expect(dateOfBirth).to.equal('1987-01-23')
+            })
+            it('must throw when the label includes the property but is not the entire word', async function(){
+                await server.setBody(`
+                    <label for="age"> Marriage: </label>
+                    <input type="text" id="age" value="18">`)
+                await user.open(server.url)
+
+                await expectToThrow('property "age" not found', async function(){
+                    await user.getValueFor('age')
+                })
+            })
+            it('must throw when the field related to the label does not exist', async function(){
+                await server.setBody(`
+                    <label for="notexistingField"> Age: </label>
+                    <input type="text" id="age" value="18">`)
+                await user.open(server.url)
+
+                await expectToThrow('missing input field for label "Age:"', async function(){
+                    await user.getValueFor('age')
+                })
+            })
+        })
+        describe('when the property is in a placeholder', function(){
+            it('must return the input value', async function(){
+                await server.setBody(`<input placeholder="age" type="text" value="18">`)
+                await user.open(server.url)
+                var age = await user.getValueFor('age')
+                expect(age).to.equal('18')
+            })
+            it('must ignore case to match property and placeholder', async function(){
+                await server.setBody(`<input placeholder="Age" type="text" value="18">`)
+                await user.open(server.url)
+                var age = await user.getValueFor('agE')
+                expect(age).to.equal('18')
             })
         })
     })
