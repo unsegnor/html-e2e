11f69c79fe131d26407c1eaaef31f4e0fc2ccf50
vcalatayud <lacuentadevictor@gmail.com>
2020-11-30 21:08:10 +0100

    Added support for fake api endpoint to the fakeserver

--- test-integration/FakeServer.js
+++ test-integration/FakeServer.js
@@ -3,14 +3,29 @@ const getPort = require('get-port')
 
 module.exports = async function(){
 
-    let settedBody
+    let settedBody, htmlFile, apiData, apiResponseTime
 
     const app = express()
     const port = await getPort()
 
     app.get('/', (req, res) => {
         if(settedBody) res.send(`<html><head></head><body>${settedBody}</body></html>`)
+        else if(htmlFile) res.sendFile(__dirname + '/' + htmlFile + '.html')
         else res.sendFile(__dirname + '/testView.html')
+    })
+
+    async function wait(milliseconds){
+        return new Promise(function(resolve){
+            setTimeout(function(){
+                resolve()
+            }, milliseconds)
+        })
+    }
+
+    app.get('/api/data', async (req, res) => {
+        if(apiResponseTime) await wait(apiResponseTime)
+        if(apiData) res.json({data:apiData})
+        else res.json({data:'defaultApiData'})
     })
 
     let server
@@ -22,13 +37,28 @@ module.exports = async function(){
             resolve(Object.freeze({
                 url,
                 close,
-                setBody
+                setBody,
+                setHtmlFile,
+                setApiData,
+                setApiResponseTime
             }))
         })
     })
 
     function setBody(body){
         settedBody = body
+    }
+
+    function setHtmlFile(filePath){
+        htmlFile = filePath
+    }
+
+    function setApiData(data){
+        apiData = data
+    }
+
+    function setApiResponseTime(milliseconds){
+        apiResponseTime = milliseconds
     }
 
     async function close(){
--- test-integration/FakeServer.spec.js
+++ test-integration/FakeServer.spec.js
@@ -1,3 +1,4 @@
+const { performance } = require('perf_hooks');
 const {expect} = require('chai')
 const FakeServer = require('./FakeServer')
 const HttpClient = require('./HttpClient')
@@ -25,5 +26,47 @@ describe('FakeServer', function(){
         var client = HttpClient()
         var content = await client.get(server.url)
         expect(content).to.contain(body)
+    })
+
+    it('must show the html file when it is set', async function(){
+        server.setHtmlFile('testHtmlFile')
+        var client = HttpClient()
+        var content = await client.get(server.url)
+        expect(content).to.contain('testHtmlFileContent')
+    })
+
+    it('must return default api data in the /api/data endpoint when it is not specified', async function(){
+        var client = HttpClient()
+        var content = await client.get(server.url + '/api/data')
+        expect(content).to.equal('{"data":"defaultApiData"}')
+    })
+
+    it('must return the specified data in the /api/data endpoint', async function(){
+        server.setApiData('testData')
+        var client = HttpClient()
+        var content = await client.get(server.url + '/api/data')
+        expect(content).to.equal('{"data":"testData"}')
+    })
+
+    it('must respond to /api/data endpoint after the response time when defined', async function(){
+        var responseTime = 1000
+        server.setApiResponseTime(responseTime)
+        var client = HttpClient()
+        var t0 = performance.now()
+        var content = await client.get(server.url + '/api/data')
+        var t1 = performance.now()
+        var actualResponseTime = t1-t0
+        expect(actualResponseTime).to.be.greaterThan(responseTime)
+        expect(content).to.equal('{"data":"defaultApiData"}')
+    })
+
+    it('must respond immediately when response time is not defined', async function(){
+        var client = HttpClient()
+        var t0 = performance.now()
+        var content = await client.get(server.url + '/api/data')
+        var t1 = performance.now()
+        var actualResponseTime = t1-t0
+        expect(actualResponseTime).to.be.lessThan(1000)
+        expect(content).to.equal('{"data":"defaultApiData"}')
     })
 })
